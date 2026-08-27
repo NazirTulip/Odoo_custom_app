abccc65e540fa202921c410a1e96f28a26b83400
NazirTulip <nazirahmed@tulip-tech.com>
2026-08-27 21:16:39 +0600

    tulip_livechat_button_color for Odoo 17.0

---
+++ tulip_livechat_button_color/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Tulip Livechat Button Hover Color',
+    'version': '17.0.1.0.0',
+    'category': 'Website/Live Chat',
+    'summary': 'Change hover color of buttons inside Live Chat to #00B3C6',
+    'description': 'Overrides hover color for all buttons inside the Odoo Live Chat embed.',
+    'author': 'Tulip-tech',
+    'website': 'https://tulip-tech.com',
+    'license': 'LGPL-3',
+    'depends': ['im_livechat'],
+    'data': [],
+    'assets': {
+        'im_livechat.assets_embed_core': [
+            'tulip_livechat_button_color/static/src/scss/livechat_button_hover.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
